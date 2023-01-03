b22eae590b6c27a32472bb986e8e97bcb98b9bc5
hanxiaorui <hanxiaorui@1623668083884741.onaliyun.com>
2023-01-03 18:56:36 +0800

    init

---
+++ src/main/java/com/xiaoruiit/technology/TechnologyApplication.java
@@ -0,0 +1,13 @@
+package com.xiaoruiit.technology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnologyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechnologyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xiaoruiit/technology/TechnologyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaoruiit.technology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnologyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
