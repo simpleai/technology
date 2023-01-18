70cff34f4d1ac5aae50c1c55f94e5b8ddf62e9bc
hanxiaorui <hanxiaorui@1623668083884741.onaliyun.com>
2023-01-18 19:27:09 +0800

    feat: 先提交事务，再执行方法内的内容

---
+++ knowledge-point/src/main/java/com/xiaoruiit/knowledge/point/transaction/ReadMe.md
@@ -0,0 +1,13 @@
+# 事务提交
+
+先提交事务，再执行方法内的内容
+
+## maven
+```xml
+<!-- 中有事务 spring-tx -->
+<dependency>
+    <groupId>org.springframework.data</groupId>
+    <artifactId>spring-data-redis</artifactId>
+</dependency>
+
+```
---
+++ knowledge-point/src/main/java/com/xiaoruiit/knowledge/point/transaction/TransactionHookProcessorTest.java
@@ -0,0 +1,21 @@
+package com.xiaoruiit.knowledge.point.transaction;
+
+/**
+ * @author hanxiaorui
+ * @date 2022/8/3
+ */
+public class TransactionHookProcessorTest {
+
+    // todo 数据库
+
+    public static void main(String[] args) {
+
+        TransactionHookProcessor transactionHookProcessor = new TransactionHookProcessor();
+
+        transactionHookProcessor.call(() ->{
+            // 发mq
+            //receProducer.send(message);
+        },true);
+
+    }
+}
