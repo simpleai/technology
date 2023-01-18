51e7ed9d9b932917be04a67afc0bc1adc3ebdef0
hanxiaorui <hanxiaorui@1623668083884741.onaliyun.com>
2023-01-18 17:44:14 +0800

    feat: 通过参数指定返回值，改变返回传输字段数量

---
+++ knowledge-point/src/main/java/com/xiaoruiit/knowledge/point/response/ResponseUserController.java
@@ -0,0 +1,24 @@
+package com.xiaoruiit.knowledge.point.response;
+
+import com.xiaoruiit.knowledge.point.common.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @author hanxiaorui
+ * @date 2022/12/28
+ */
+@RestController()
+@RequestMapping("/response/user")
+public class ResponseUserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/code/{code}")
+    public Result<List<?>> findByCode(@PathVariable("code") String code, @RequestParam("returnType") UserInfoDtoEnum returnType) {
+        return userService.findByCode(code, returnType);
+    }
+}
