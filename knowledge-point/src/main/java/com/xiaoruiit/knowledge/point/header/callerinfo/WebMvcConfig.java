c34eb50b46655ad99aa2c1f16e7f8ee493c0cc18
hanxiaorui <hanxiaorui@1623668083884741.onaliyun.com>
2023-01-18 15:47:27 +0800

    feat: 公共调用者信息传递与获取

---
+++ knowledge-point/src/main/java/com/xiaoruiit/knowledge/point/header/callerinfo/WebMvcConfig.java
@@ -0,0 +1,30 @@
+package com.xiaoruiit.knowledge.point.header.callerinfo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author hanxiaorui
+ * @date 2022/4/9
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Bean
+    public CallerInfoInterceptor requestHeaderContextInterceptor() {
+        return new CallerInfoInterceptor();
+    }
+
+    /**
+     * 绑定拦截器
+     * @param registry
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(requestHeaderContextInterceptor())
+                .addPathPatterns("/**");// 拦截所有请求
+        WebMvcConfigurer.super.addInterceptors(registry);
+    }
+}
