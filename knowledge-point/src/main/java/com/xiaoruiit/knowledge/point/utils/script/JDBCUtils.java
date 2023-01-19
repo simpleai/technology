5b42653f95645e38dbbcab2e1fb920a6d2493d88
hanxiaorui <hanxiaorui@1623668083884741.onaliyun.com>
2023-01-19 19:03:18 +0800

    feat: jdbcTemplete

---
+++ knowledge-point/src/main/java/com/xiaoruiit/knowledge/point/utils/script/JDBCUtils.java
@@ -0,0 +1,35 @@
+package com.xiaoruiit.knowledge.point.utils.script;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+
+/**
+ * JdbcTemplate工具类 可用于脚本
+ *
+ * @author hxr
+ */
+
+@Slf4j
+public class JDBCUtils {
+    //  type: com.alibaba.druid.pool.DruidDataSource
+    //    driverClassName: com.mysql.cj.jdbc.Driver
+    static String url = "jdbc:mysql://127.0.0.1:3306?useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&useSSL=true";
+    static String username = "root";
+    static String password = "123456";
+
+    public static JdbcTemplate getJdbcTemplate(){
+        // Spring 内置连接池
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        // 需要 jdbc 四个参数
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        return jdbcTemplate;
+    }
+
+}
---
+++ knowledge-point/src/main/java/com/xiaoruiit/knowledge/point/utils/script/ReadMe.md
@@ -0,0 +1,12 @@
+# 获取JDBCTemplete
+
+## 导包
+
+```xml
+<!-- 实测5.3.4 -->
+<dependency>
+    <groupId>org.springframework</groupId>
+    <artifactId>spring-jdbc</artifactId>
+</dependency>
+```
+
